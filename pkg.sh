45e536f270a8e16952fbb2b40c52683f5a5b9f3a
Len <cxin1314@gmail.com>
2018-04-19 02:10:01 +0800

    pkg

--- pkg.sh
+++ pkg.sh
@@ -8,8 +8,6 @@ then
     echo "$ pkg <IOS打包版本>"
 elif [ $1 == "path" ]
 then
-    # sed -i '2c cREPLACE LINE' pkg
-    # sed -i ".bak" '2c\'$'\nios_path=\n' pkg.sh
     sed -i ".bak" '2c\'$'\nios_path='${2}$'\n' ~/pkg.sh
 else
     cd ${ios_path}/buglySymboliOS2.4.3 && sh -x buglySymboliOS.sh ${ios_path}/BonDay.app.dSYM -o ${ios_path}/bugly."$1".zip
