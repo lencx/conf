c8f6eedb62091a0eaaf5fce4565f3291fda1870f
Len <cxin1314@gmail.com>
2018-04-19 15:35:43 +0800

    pkg

--- pkg.sh
+++ pkg.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-ios_path="~"
+sh_path=~/pkg.sh
+app_name="app"
+proj_path="~"
+pkg_path="~"
+bugly_dir="bugly"
 
 if [ -z $1 ]
 then
-    echo "$ pkg path <项目路径(绝对路径)>"
-    echo "$ pkg <IOS打包版本>"
+    echo "  $ pkg path <APP Name> <Project Path> <Package Path> <Bugly Dir>         (Absolute path)"
+    echo "  $ pkg <Version Number>"
 elif [ $1 == "path" ]
 then
-    sed -i ".bak" '2c\'$'\nios_path='${2}$'\n' ~/pkg.sh
+    sed -i ".bak" '4c\'$'\napp_name='$2$'\n' $sh_path
+    sed -i ".bak" '5c\'$'\nproj_path='$3$'\n' $sh_path
+    sed -i ".bak" '6c\'$'\npkg_path='$4$'\n' $sh_path
+    sed -i ".bak" '7c\'$'\nbugly_dir='$5$'\n' $sh_path
 else
-    cd ${ios_path}/buglySymboliOS2.4.3 && sh -x buglySymboliOS.sh ${ios_path}/BonDay.app.dSYM -o ${ios_path}/bugly."$1".zip
+    cd ${proj_path}/${bugly_dir} && sh -x buglySymboliOS.sh ${proj_path}/${app_name}.app.dSYM -o ${pkg_path}/bugly."$1".zip
 fi
