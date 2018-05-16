dc7d00a8383ad51d9f8d0ddbeca96edddb99fb64
Len <cxin1314@gmail.com>
2018-05-16 13:50:01 +0800

    search

--- search_terminal.sh
+++ search_terminal.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+# Usage:
+# echo alias s=\". ~/search_terminal.sh\" >> ~/.zshrc
+
+searchWord=""
+s1=\#en/zh-CN/
+
 if [ "$1" == "-h" ];then
     echo "usage:"
-    echo "  \e[1;32m-b    \e[1;31mBaidu"
-    echo "  \e[1;32m-B    \e[1;31mBing"
-    echo "  \e[1;32m-g    \e[1;31mGoogle"
-    echo "  \e[1;32m-gm   \e[1;31mGmail"
-    echo "  \e[1;32m-git  \e[1;31mGithub"
-    echo "  \e[1;32m-n    \e[1;31mnpm"
-    echo "  \e[1;32m-o    \e[1;31mOther"
-    echo "  \e[1;32m-tc   \e[1;31mCN => EN"
-    echo "  \e[1;32m-te   \e[1;31mEN => CN\e[0m"
+    echo "   \033[32m-b    \033[31mBaidu"
+    echo "   \033[32m-B    \033[31mBing"
+    echo "   \033[32m-g    \033[31mGoogle"
+    echo "   \033[32m-gm   \033[31mGmail"
+    echo "   \033[32m-git  \033[31mGithub"
+    echo "   \033[32m-n    \033[31mnpm"
+    echo "   \033[32m-o    \033[31mOther"
+    echo "   \033[32m-t    \033[31mCN => EN\033[0m"
 else
     case $1 in
-        -b) web='https://www.baidu.com/s?wd=';;
-        -B) web='https://www.bing.com/search?q=';;
-        -g) web='https://www.google.com/search?q=';;
-        -gm) web='https://mail.google.com/mail/u/0/#inbox';;
-        -git) web='https://github.com/search?q=';;
-        -n) web='https://www.npmjs.com/search?q=';;
-        -o) web='http://';;
-        -tc) web='https://translate.google.cn/#zh-CN/en/';;
-        -te) web='https://translate.google.cn/#en/zh-CN/';;
-        *) web='https://www.google.com/search?q=';;
+        -b) web=https://www.baidu.com/s?wd=;;
+        -B) web=https://www.bing.com/search?q=;;
+        -g) web=https://www.google.com/search?q=;;
+        -gm) web=https://mail.google.com/mail/u/0/'#'inbox;;
+        -git) web=https://github.com/search?q=;;
+        -n) web=https://www.npmjs.com/search?q=;;
+        -o) web=http://;;
+        -t) web=https://translate.google.cn/$s1;;
+        *) web=https://www.google.com/search?q=;;
     esac
 
     for item in $@
     do
-        if [[ "$item" != "-b" && "$item" != "-B" && "$item" != "-g" && "$item" != "-gm" && "$item" != "-git" && "$item" != "-n" && "$item" != "-o" && "$item" != "-tc" && "$item" != "-te" ]];then
+        if [[ "$item" != "-b" && "$item" != "-B" && "$item" != "-g" && "$item" != "-gm" && "$item" != "-git" && "$item" != "-n" && "$item" != "-o" && "$item" != "-t" ]];then
             if [ "$searchWord" == "" ];then
                 searchWord="$item"
             else
@@ -35,7 +39,7 @@ else
             fi
         fi
     done;
-    
+
     echo "Loading ..."
-    open "${web}${searchWord}"
+    open ${web}${searchWord}
 fi
